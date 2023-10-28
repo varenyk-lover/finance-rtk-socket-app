760e92c27e4d9f3f20a2ce3e13e68ebdce000a55
Hanna <hanna.horbova@gmail.com>
2023-10-28 11:30:23 +0300

    update data

--- client/src/App.js
+++ client/src/App.js
@@ -46,7 +46,6 @@ const App = () => {
             socket.connect();
             socket.emit("start");
         }
-        console.log(newIsTickerOn);
     };
 
     return (
--- client/src/components/FinanceTable/FinanceTable.test.js
+++ client/src/components/FinanceTable/FinanceTable.test.js
@@ -9,11 +9,12 @@ describe("FinanceTable component", () => {
         {
             ticker: "AAPL",
             exchange: "NASDAQ",
-            price: 123.45,
-            change: 1.23,
-            change_percent: 1.0,
-            dividend: 0.5,
-            yield: 1.5,
+            price: 279.29,
+            change: 64.52,
+            change_percent: 0.84,
+            dividend: 0.56,
+            yield: 1.34,
+            last_trade_time: "2021-04-30T11:53:21.000Z"
         },
     ];
 
